40c3a0a3da272538791359d6541532cf2a451e56
markreinerpanlilio <markreinerpanlilio@gmail.com>
2026-07-14 23:18:55 +0800

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://pcpahrtrnynjdnjptqwh.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjcGFocnRybnluamRuanB0cXdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNDk4OTgsImV4cCI6MjA5MjgyNTg5OH0.wc0kRk92aC7l0cV6kg30v5ApggVRlzwkPL4JHrdvxHU";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
